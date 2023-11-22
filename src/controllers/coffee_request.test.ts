bae80a16edbfec7f67ee58b20de5581645f7274d
netcam22 <annettelesage@btinternet.com>
2023-11-22 08:20:44 +0000

    feat: Implement coffee lover endpoint test

--- src/controllers/coffee_request.test.ts
+++ src/controllers/coffee_request.test.ts
@@ -1,9 +1,9 @@
 import request from "supertest";
 import { app } from "../app";
-describe("Test home API endpoint request", () => {
+describe("Test Coffee API endpoint request", () => {
   test("GET should return correct message", async () => {
-    const res = await request(app).get("/");
+    const res = await request(app).get("/coffeelover");
     expect(res.statusCode).toEqual(200);
-    expect(res.text).toEqual("Welcome to the Drinks API!");
+    expect(res.text).toEqual("I like coffee!");
   });
 });
